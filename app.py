489bf4e4d6c2998ff1f0a6e55cda93abe43531e7
Dhanu Ramesh <dhanuramesh2509@gmail.com>
2026-07-16 19:33:38 +0530

    Rename folders for Linux compatibility

--- app.py
+++ app.py
@@ -597,8 +597,10 @@ def download_report():
     return send_file(pdf_file, as_attachment=True)
 
 
+import os
+
 if __name__ == "__main__":
- app.run(
-    host="0.0.0.0",
-    port=int(os.environ.get("PORT", 5000))
-)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
